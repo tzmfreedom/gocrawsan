414ba103d50b6ec9ce0c1582f944e58fb7bad818
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2017-04-23 00:15:47 +0900

    Add getLink function

--- main.go
+++ main.go
@@ -6,25 +6,31 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 	"sync"
 
 	"github.com/BurntSushi/toml"
-	_ "github.com/PuerkitoBio/goquery"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/fatih/color"
 	"github.com/mitchellh/go-homedir"
 	"github.com/urfave/cli"
 )
 
 var (
-	Version   string
-	Revision  string
-	useragent string = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1)"
+	Version  string
+	Revision string
 )
 
 type configFile struct {
-	Urls []string `toml:"urls"`
+	Urls      []string `toml:"urls"`
+	UserAgent string   `toml:"useragent"`
+}
+
+type requestSetting struct {
+	url       string
+	useragent string
 }
 
 func main() {
@@ -46,7 +52,7 @@ func main() {
 	app.Usage = ""
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name: "urls",
+			Name: "useragent, U",
 		},
 		cli.StringFlag{
 			Name:  "config, C",
@@ -63,36 +69,44 @@ func main() {
 func crawling(c *cli.Context) error {
 	wg := new(sync.WaitGroup)
 	m := new(sync.Mutex)
+
+	ua := c.String("useragent")
 	buf, err := ioutil.ReadFile(c.String("config"))
 	if err != nil {
 		return err
 	}
 	file := &configFile{}
 	err = toml.Unmarshal(buf, file)
+	if err != nil {
+		return err
+	}
 
 	for _, url := range file.Urls {
 		wg.Add(1)
-		go getUrl(wg, m, url)
+		s := &requestSetting{
+			url:       url,
+			useragent: ua,
+		}
+		go getUrl(wg, m, s)
 	}
 	wg.Wait()
 	return nil
 }
 
-func getUrl(wg *sync.WaitGroup, m *sync.Mutex, url string) {
+func getUrl(wg *sync.WaitGroup, m *sync.Mutex, s *requestSetting) {
 	client := &http.Client{}
 	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
 		return http.ErrUseLastResponse
 	}
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Set("User-Agent", useragent)
+	req, _ := http.NewRequest("GET", s.url, nil)
+	req.Header.Set("User-Agent", s.useragent)
 
 	resp, _ := client.Do(req)
 	status := strings.Split(resp.Status, " ")
 	code, _ := strconv.Atoi(status[0])
 
 	m.Lock()
-	defer m.Unlock()
-	fmt.Print(url + "\t")
+	fmt.Print(s.url + "\t")
 
 	switch code / 100 {
 	case 2:
@@ -104,5 +118,21 @@ func getUrl(wg *sync.WaitGroup, m *sync.Mutex, url string) {
 	default:
 		fmt.Println(resp.Status)
 	}
+	m.Unlock()
+	_, _ := getLinks(resp)
 	wg.Done()
+}
+
+func getLinks(res *http.Response) ([]string, error) {
+	urls := []string{}
+	doc, _ := goquery.NewDocumentFromResponse(res)
+	doc.Find("a").Each(func(_ int, s *goquery.Selection) {
+		url, _ := s.Attr("href")
+		r := regexp.MustCompile(`^(https|http)://(.*)`)
+		if !r.MatchString(url) {
+			url = res.Request.URL.Scheme + "://" + res.Request.URL.Host + url
+		}
+		urls = append(urls, url)
+	})
+	return urls, nil
 }
